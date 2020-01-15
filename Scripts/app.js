d0d360397c0b4a2a5abb7bd5e2ba4bf8d1f2537e
Jasim Khan <jasim.khan89@gmail.com>
2020-01-14 19:51:22 -0500

    added Bootstrap and Font-Awesome

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,30 @@
 // IIFE -  Immediately Invoked Function Expression
+//anonymous self-executing function
 (function(){
 
         function Start()
             {
-                console.log("App Started..");    
+                console.log("App Started..");
+                
+               let firstHeading = document.getElementById("firstHeading");
+
+                firstHeading.textContent = "Goodbye cruel world!"; 
+
+                // document.getElementById("firstHeading").textContent = "Goodbye cruel world!!!";
+                
+                firstHeading.style.fontWeight = "bold";
+                
+                let main = document.getElementsByTagName("main")[0];
+
+                //configuring new element
+                let newH2 = document.createElement("h2");
+                newH2.setAttribute("class","Display-5");
+                newH2.textContent = "secondary heading";
+
+                main.appendChild(newH2);
+            
+            
+            
             }           
 
     window.addEventListener("load", Start);
